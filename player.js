5edf6c9969319db759b855b31b56ac36443b8c91
ArnavBhattacharya <70324531+ArnavBhattacharya@users.noreply.github.com>
2020-11-25 18:02:20 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,28 @@
+class Player {
+    constructor(){
+      this.index = null;
+      this.distance = 0;
+      this.name = null;
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+        playerCount: count
+        });
+    }
+
+    updateCode(code){
+        database.ref('/').update({
+            saveCode: code
+        });
+    }
+
+    getCount(){
+        database.ref('playerCount').on("value", (data) =>{
+            playerCount = data.val();
+        });
+    }
+  
+
+
+}
